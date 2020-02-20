71ebdf5fd38a7c6e9f682cf023a595c5afcb6f2e
Dahl_Cloud9_Workspace <pdahl@csumb.edu>
2020-02-20 19:49:06 +0000

    updated links on homepage

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+// app.use(express.static('/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -17,7 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- index.php
+++ index.php
@@ -3,10 +3,21 @@
 <head>
 <meta charset="UTF-8">
 <title>CST336 </title>
+<link rel="stylesheet" type="text/css" href="/public/css/style.css" />
 </head>
 
 <body>
     <h1> Pernille's CST336 Homepage </h1>
+    
+    <div class="lab"> 
+    <h3> Labs </h3>
+        <a href="http://pdahl95-cst336-spring2020.herokuapp.com/labs/1/"> Lab 1 - Portfolio </a> <br>
+        <a href="http://pdahl95-cst336-spring2020.herokuapp.com/labs/2/"> Lab 2 - Guess Number Game</a><br>
+        <a href="http://pdahl95-cst336-spring2020.herokuapp.com/cst336_s20_labs/lab3/"> Lab 3 - US Quiz</a><br>
+        <a href="http://pdahl95-cst336-spring2020.herokuapp.com/labs/4/"> Lab 4 - AJAX Sign Up </a><br>
+        
+    </div>
+    
 </body>
 
 </html>
